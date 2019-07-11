7eb2869b3b66fdbaf52abdc4f7ced555b6637ba2
Marcos <Marcos@DESKTOP-OJ7Q8NM>
2019-07-11 00:18:04 -0300

    Versão 1.7 11/07/19 00:17

--- Datasalles/src/main/java/br/com/datasalles/Bean/NfiscalBean.java
+++ Datasalles/src/main/java/br/com/datasalles/Bean/NfiscalBean.java
@@ -11,7 +11,10 @@ import javax.faces.event.ActionEvent;
 import org.omnifaces.util.Messages;
 
 import br.com.datasalles.dao.NfiscalDAO;
+import br.com.datasalles.domain.Compra;
+import br.com.datasalles.domain.Fornecedor;
 import br.com.datasalles.domain.Nfiscal;
+
 
 @SuppressWarnings("serial")
 @ManagedBean
@@ -19,6 +22,8 @@ import br.com.datasalles.domain.Nfiscal;
 public class NfiscalBean implements Serializable {
 	private Nfiscal nfiscal;
 	private List<Nfiscal>nfiscals;
+	private List<Compra>compras;
+	private List<Fornecedor>fornecedores;
 	
 	public Nfiscal getNfiscal() {
 		return nfiscal;
@@ -28,14 +33,29 @@ public class NfiscalBean implements Serializable {
 		this.nfiscal = nfiscal;
 	}
 	
-	public List<Nfiscal> getNfiscales() {
+	public List<Nfiscal> getNfiscals() {
 		return nfiscals;
 	}
 	
-	public void setNfiscales(List<Nfiscal> nfiscals) {
+	public void setNfiscals(List<Nfiscal> nfiscals) {
 		this.nfiscals = nfiscals;
 	}
 	
+	public List<Compra> getCompras() {
+		return compras;
+	}
+	
+	public void setCompras(List<Compra> compras) {
+		this.compras = compras;
+	}
+	
+	public List<Fornecedor> getFornecedores() {
+		return fornecedores;
+	}
+	
+	public void setFornecedores(List<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
 
 	public void novo() {
 		nfiscal = new Nfiscal();
